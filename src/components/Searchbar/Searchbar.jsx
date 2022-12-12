03fdd5da200b07d63f0fc72aa935ecb65dfed386
Екатерина <Katderl@ukr.net>
2022-12-13 00:49:56 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { ImSearch } from 'react-icons/im';
 import { toast } from 'react-toastify';
@@ -5,30 +6,28 @@ import css from './Searchbar.module.css'
 
 const styles = { form: { marginBottom: 20 } };
 
-export class Searchbar extends Component {
-  state = {
-    searchQuery: '',
+export function Searchbar ({ onSubmit })  {
+  
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const onChangeInput = event => {
+    setSearchQuery(event.currentTarget.value.toLowerCase());
   };
 
-  onChangeInput = event => {
-    this.setState({ searchQuery: event.currentTarget.value.toLowerCase() });
-  };
-
-  onSubmitForm = event => {
+  const onSubmitForm = event => {
     event.preventDefault();
 
-    if (this.state.searchQuery.trim() === '') {
+    if (searchQuery.trim() === '') {
     toast.error('Enter a search term.');
       return;
     }
-    this.props.onSubmit(this.state.searchQuery);
-    this.setState({ searchQuery: '' });
+    onSubmit(searchQuery);
+    setSearchQuery('');
   };
 
-  render() {
     return (
       <header className={css.header}>
-      <form className={css.form} onSubmit={this.onSubmitForm} style={styles.form}>
+      <form className={css.form} onSubmit={onSubmitForm} style={styles.form}>
         <button type="submit"
                 className={css.button}>
 
@@ -42,14 +41,16 @@ export class Searchbar extends Component {
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
-            value={this.state.searchQuery}
-            onChange={this.onChangeInput}
+            value={searchQuery}
+            onChange={onChangeInput}
           />
       </form>
       </header>
     );
   }
-}
 
 
+  Searchbar.propTypes = {
+    onSubmit: PropTypes.func.isRequired
+  };
 
