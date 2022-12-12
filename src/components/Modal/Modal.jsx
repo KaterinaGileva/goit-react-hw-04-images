4bc55b14de53c87b0595c002f42d5b2d45fe51b4
Екатерина <Katderl@ukr.net>
2022-12-13 00:40:18 +0200

    summary

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,44 +1,33 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  static propTypes = {
-    title: PropTypes.string,
-    onClose: PropTypes.func.isRequired,
-    currentImageUrl: PropTypes.string,
-    currentImageDescription: PropTypes.string.isRequired,
-  };
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
+export function Modal({ onClose, currentImageUrl, currentImageDescription }) {
+  useEffect(() => {
+    const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
+    window.addEventListener('keydown', handleKeyDown);
+  
+  return () => {window.removeEventListener('keydown', handleKeyDown)};
+}, [onClose]
+  )
 
-  handleClickBackdrop = e => {
+  const handleClickBackdrop = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const { currentImageUrl, currentImageDescription } =
-      this.props;
+ 
 
     return createPortal(
-      <div className={css.backdrop} onClick={this.handleClickBackdrop}>
+      <div className={css.backdrop} onClick={handleClickBackdrop}>
         <div className={css.modal}>
           
           <img
@@ -51,4 +40,11 @@ export class Modal extends Component {
       modalRoot
     );
   }
-}
+
+
+Modal.prototype = {
+ 
+  onClose: PropTypes.func.isRequired,
+  currentImageUrl: PropTypes.string,
+  currentImageDescription: PropTypes.string.isRequired,
+};
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { React, Component } from 'react';
+import { useState } from 'react';
 import { ImSearch } from 'react-icons/im';
 import { toast } from 'react-toastify';
 import css from './Searchbar.module.css'
